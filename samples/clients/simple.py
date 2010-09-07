94b5d2dbc89bbda9f5d6c24bebf2f0e0fe461bb4
Mario Frasca <mariotomo@gmail.com>
2010-09-07 08:51:51 +0000

    sample clinet simple.py now uses a different sample server.

--- samples/clients/simple.py
+++ samples/clients/simple.py
@@ -1,16 +1,22 @@
 #!/usr/bin/env python
-from PyOPC.OPCContainers import *
+from PyOPC.OPCContainers import ItemContainer
 from PyOPC.XDAClient import XDAClient
 
-def print_options((ilist,options)):
-    print ilist; print options; print
-    
-address='http://violin.qwer.tk:8000/'
+# address='http://violin.qwer.tk:8000/'
+address='http://www.advosol.us/xmldademo/xml_sim/OpcXmlDaServer.asmx'
 
-xda = XDAClient(OPCServerAddress=address,
-                ReturnErrorText=True)
+xda = XDAClient(OPCServerAddress=address, ReturnErrorText=True)
 
-print_options(xda.GetStatus())
-print_options(xda.Browse())
-print_options(xda.Read([ItemContainer(ItemName='simple_item', MaxAge=500)],
-                       LocaleID='en-us'))
+print xda.GetStatus()
+print xda.Browse()
+
+# hard coded request for http://violin.qwer.tk:8000/.
+# xda.Read([ItemContainer(ItemName='simple_item', MaxAge=500)], LocaleID='en-us')
+
+print "querying one item at a time"
+requestable = [i for i in xda.Browse()[0] if i.IsItem is True]
+for name in [i.ItemName for i in requestable]:
+    print xda.Read([ItemContainer(ItemName=name, MaxAge=5000)], LocaleID='en-us')
+
+print "querying all items simultaneously"
+print xda.Read(requestable)
